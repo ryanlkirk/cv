7591b47173e3a65e745e7431f0d175a7f1592140
Ryan Kirk <ryanlkirk@live.co.uk>
2023-04-30 21:18:07 +0100

    Add dark mode functionality

--- script.js
+++ script.js
@@ -1,19 +1,77 @@
+let viewMode = 0;
+
 let projectVisibility = {
-    twitch: false,
-    sp: false,
-    jun: false,
-    otrack: false
-  };
+  twitch: false,
+  sp: false,
+  jun: false,
+  otrack: false
+};
+
+getClasses = (ele) => {
+  return ele.classList;
+}
+
+toggleProject = (projectId, btnId) => {
+  const project = document.getElementById(projectId);
+  const btn = document.getElementById(btnId);
+  projectVisibility[projectId] = !projectVisibility[projectId];
+  btn.innerText = projectVisibility[projectId] ? "Hide Project" : "View Project";
+  const classes = getClasses(project);
+  projectVisibility[projectId] ? classes.remove("display-none") : classes.add("display-none");
+}
+
+toggleDarkMode = () => {
+  viewMode = !viewMode ? 1 : 0;
+  const allJobBlocks = document.getElementsByClassName("job-block job-bg");
+  const allQuotes = document.getElementsByClassName("quote");
+  const allTitles = document.getElementsByClassName("experience-title lrg-text");
+  const allSubTitles = document.getElementsByClassName("job-title");
+  const allDates = document.getElementsByClassName("date-range");
+  const allProjectAbout = document.getElementsByClassName("experience-title lrg-text-sub");
+
+  processClassChange(document.getElementsByClassName("right-experience w70")[0], "dark-bg");
   
-  getClasses = (ele) => {
-    return ele.classList;
+  processClassChange(document.getElementsByClassName("left-about w30")[0], "dark-contrast-bg");
+
+  processClassChange(document.getElementsByClassName("right-experience w70")[0], "light-text");
+
+  processClassChange(document.getElementsByClassName("dark-mode")[0], "dark-bg");
+  processClassChange(document.getElementsByClassName("dark-mode")[0], "light-text");
+
+  processClassChange(document.getElementsByClassName("experience-heading")[0], "light-text");
+
+  processClassChange(document.getElementsByClassName("quote")[0], "quote-text");
+
+  Array.from(allJobBlocks).forEach(el => {
+    processClassChange(el, "dark-contrast-bg");
+  });
+
+  Array.from(allQuotes).forEach(el => {
+    processClassChange(el, "quote-text");
+  });
+
+  Array.from(allTitles).forEach(el => {
+    processClassChange(el, "light-text");
+  });
+
+  Array.from(allSubTitles).forEach(el => {
+    processClassChange(el, "light-text");
+  });
+
+  Array.from(allDates).forEach(el => {
+    processClassChange(el, "light-text");
+  });
+
+  Array.from(allProjectAbout).forEach(el => {
+    processClassChange(el, "light-text");
+  });
+
+}
+
+processClassChange = (el, newStyle) => {
+  if (viewMode) {
+    el.classList.add(newStyle)
+  } else {
+    el.classList.remove(newStyle)
   }
-  
-  toggleProject = (projectId, btnId) => {
-    const project = document.getElementById(projectId);
-    const btn = document.getElementById(btnId);
-    projectVisibility[projectId] = !projectVisibility[projectId];
-    btn.innerText = projectVisibility[projectId] ? "Hide Project" : "View Project";
-    const classes = getClasses(project);
-    projectVisibility[projectId] ? classes.remove("display-none") : classes.add("display-none");
-  }
+}
